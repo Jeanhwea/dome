afb6748e57989442fbaca509cbee7ced92c01a24
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-15 00:47:41 +0800

    update: 添加安装包

--- setup/mac-brew-install-packages.sh
+++ setup/mac-brew-install-packages.sh
@@ -1,10 +1,87 @@
 DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
+DOME_BREW_PACKAGES=(
+    "binutils"
+    "coreutils"
+    "dateutils"
+    "diffutils"
+    "findutils"
+    "gputils"
+    "icoutils"
+    "inetutils"
+    "moreutils"
+    "aria2"
+    "asciidoctor"
+    "bash-completion"
+    "ccat"
+    "cheat"
+    "cloc"
+    "editorconfig"
+    "elm"
+    "ffmpeg"
+    "fx"
+    "git"
+    "git-lfs"
+    "git-open"
+    "global"
+    "gnu-sed"
+    "graphviz"
+    "grip"
+    "htop"
+    "httpie"
+    "jq"
+    "lrzsz"
+    "lua"
+    "luarocks"
+    "maven"
+    "mplayer"
+    "ncdu"
+    "nmap"
+    "node"
+    "p7zip"
+    "pandoc"
+    "pgformatter"
+    "protobuf"
+    "pv"
+    "python"
+    "python3"
+    "r"
+    "ranger"
+    "readline"
+    "reattach-to-user-namespace"
+    "rg"
+    "rlwrap"
+    "ruby"
+    "sqlparse"
+    "the_platinum_searcher"
+    "the_silver_searcher"
+    "tmux"
+    "tree"
+    "unar"
+    "unrar"
+    "wget"
+    "yarn"
+    "you-get"
+    "z"
+    "zsh-autosuggestions"
+)
 
 dome_brew_install() {
-    local pkgs=$*
-    dome_exec brew install $pkgs
+    local pkg=$*
+    local version=$(brew ls --versions "$pkg")
+
+    if [ X"$version" == X"" ]; then
+        dome_exec brew install $pkg
+    else
+        logi "Find $version"
+    fi
 }
 
-dome_brew_install vlc 
+dome_install_packages() {
+    for pkg in "${DOME_BREW_PACKAGES[@]}"; do
+        dome_brew_install $pkg
+    done
+}
+
+dome_install_packages
