8d9a9fe535c6ba173e6e40a214cb78b8390e8cbc
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-15 01:44:04 +0800

    add: golang envs

--- env/setup-golang.sh
+++ env/setup-golang.sh
@@ -1,2 +1,9 @@
 DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
 . $DOME_BASE/common/common.sh
+
+# golang
+export GOROOT=${GOROOT:-/usr/local/go}
+export GOPATH=${GOPATH:-$HOME/go}
+export CGO_ENABLED=1
+
+export PATH="$GOROOT/bin:$PATH"
