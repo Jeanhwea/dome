957981130270fc7aede4ef6f969b01d114c6552c
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-15 02:10:09 +0800

    update: 添加更多 zsh 的配置

--- env/setup-zsh-tools.sh
+++ env/setup-zsh-tools.sh
@@ -1,3 +1,22 @@
-DOME_ZA=/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
+. $DOME_BASE/common/common.sh
 
+# zsh-autosuggestions.zsh
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=yellow'
+
+DOME_ZA=/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 [ -f $DOME_ZA ] && source $DOME_ZA
+
+
+
+# change homebrew bottle domain
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+
+# taobao mirror for npm
+# export CHROMEDRIVER_CDNURL=http://npm.taobao.org/mirrors/chromedriver
+# export ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
+# export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
+# export PHANTOMJS_CDNURL=http://npm.taobao.org/mirrors/phantomjs
+# export SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass
+# export SELENIUM_CDNURL=http://npm.taobao.org/mirrorss/selenium
+# export SQLITE3_BINARY_SITE=http://npm.taobao.org/mirrors/sqlite3
