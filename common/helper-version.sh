f50af19c39c644d698c15b3e16e3c6360790e3ba
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-15 15:34:29 +0800

    update: 整理版本提升的函数

--- common/helper-version.sh
+++ common/helper-version.sh
@@ -1,23 +1,16 @@
 DOME_COLOR_ECHO="echo"
 
-split_version() {
-    local verstr=${1/v/}
-    local vernums=($(echo $verstr | tr "." "\n"))
-    echo ${vernums[@]}
-}
-
 bomup_version() {
     local verstr=$1
 
-    local leftCount=3
+    local count=0
     if [ $# -gt 1 ]; then
-        local leftCount=$2
+        local count=$2
     fi
 
-    local vernums=$(split_version $verstr)
+    local vernums=($(echo ${verstr/v/} | tr "." "\n"))
 
-    local ansstr=
-
+    local ans=""
     local i=1
     for num in ${vernums[@]}; do
         local prefix="v"
@@ -26,23 +19,24 @@ bomup_version() {
         fi
 
         local curr=0
-        if [ $i -lt  $leftCount ]; then
+        if [ $i -lt  $count ]; then
             curr=$num
-        elif [ $i -eq  $leftCount ]; then
+        elif [ $i -eq  $count ]; then
             curr=$((num+1))
         fi
 
-        ansstr="${ansstr}${prefix}${curr}"
+        ans="${ans}${prefix}${curr}"
 
         ((i++))
     done
 
-    echo $ansstr
+    echo $ans
 }
 
-# bomup_version "v1.2.3"
-# bomup_version "v1.2.3" 0
-# bomup_version "v1.2.3" 1
-# bomup_version "v1.2.3" 2
-# bomup_version "v1.2.3" 3
-# bomup_version "v1.2.3" 4
+bomup_version "v1.2.3"
+bomup_version "v1.2.3" 0
+bomup_version "v1.2.3" 1
+bomup_version "v1.2.3" 2
+bomup_version "v1.2.3" 3
+bomup_version "v1.2.3" 4
+bomup_version "v1.2.3.4" 4
