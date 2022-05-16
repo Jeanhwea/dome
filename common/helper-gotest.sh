def3fdf04c72023ae618c8dec61f46d2d1ca227a
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-16 17:11:58 +0800

    add: 添加录制鼠标点击的接入

--- common/helper-gotest.sh
+++ common/helper-gotest.sh
@@ -1,15 +1,7 @@
-DOME_PUMS_SCRIPT_DIR=$HOME/work/pums/script_tools
-
 gotest_run_testcase() {
     local dir=$1
     local testcase=$2
 
-    cd $1 && dome_exec go test -v -count=1 -timeout=0 -run $testcase .
-}
-
-gotest_run_pums_testcase() {
-    local dir=$DOME_PUMS_SCRIPT_DIR/run
-    local testcase=$1
-
-    gotest_run_testcase $dir $testcase
+    cd $1
+    dome_exec go test -v -count=1 -timeout=0 -run $testcase .
 }
--- script/bot-record-mouse-clicks.sh
+++ script/bot-record-mouse-clicks.sh
@@ -1,2 +1,12 @@
 DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
 . $DOME_BASE/common/common.sh
+
+
+gotest_run_pums_testcase() {
+    local dir=$HOME/work/pums/script_tools/run
+    local testcase=$1
+
+    gotest_run_testcase $dir $testcase 2>/dev/null | grep "RecordMouseAction"
+}
+
+gotest_run_pums_testcase TestBotCommonScripts01_RecordMouseAction
