1535bb48ea42f07356cfc02da23f69754ff8fd0e
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-17 11:39:15 +0800

    update: 完成记录鼠标的脚本集成

--- script/bot-record-mouse-clicks.sh
+++ script/bot-record-mouse-clicks.sh
@@ -2,11 +2,11 @@ DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 
-gotest_run_pums_testcase() {
-    local dir=$HOME/work/pums/script_tools/run
+gotest_run_renyin_testcase() {
+    local dir=$HOME/code/jeanhwea/renyin/run
     local testcase=$1
 
     gotest_run_testcase $dir $testcase 2>/dev/null | grep "RecordMouseAction"
 }
 
-gotest_run_pums_testcase TestBotCommonScripts01_RecordMouseAction
+gotest_run_renyin_testcase TestBotScripts01_RecordMouseAction
