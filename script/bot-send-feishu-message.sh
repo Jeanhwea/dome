6701d36517713eddc4a90c954395c62f98187070
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-17 23:02:36 +0800

    update: 添加消息管理

---
+++ script/bot-send-feishu-message.sh
@@ -0,0 +1,17 @@
+DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
+. $DOME_BASE/common/common.sh
+
+gotest_run_renyin_testcase() {
+    local dir=$HOME/code/jeanhwea/renyin
+    local testcase=$1
+
+    local name="bot-message-list"
+    if [ $# -ge 2 ]; then
+        local name=${2/.csv/}
+    fi
+
+    cd $dir
+    dome_exec go test -v -count=1 -timeout=0 -run $testcase ./run -args "${name}.csv"
+}
+
+gotest_run_renyin_testcase TestFeishuScripts01_SendMessage $*
