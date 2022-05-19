4e988dfb14341446ffac2f5a95edc0c3f175a252
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-19 11:12:17 +0800

    update: 添加格式化 goland 代码

---
+++ script/golang-format-source-here.sh
@@ -0,0 +1,13 @@
+DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
+. $DOME_BASE/common/common.sh
+
+
+dome_format_golans_source() {
+    local files=$(find . -iname '*.go' -not -path "./*_gen/*")
+    for file in ${files[@]}; do
+        dome_exec sed -i '/^import/,/^\s*)/ { /^\s*$/ d; }' $file
+        dome_exec goimports -w $file
+    done
+}
+
+dome_format_golans_source $*
