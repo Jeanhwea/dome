8c566d0e6a0ed57dd3ae535f63d9ccf20ac6230c
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-22 02:52:00 +0800

    polish

--- script/git-init-private-repository.sh
+++ script/git-init-private-repository.sh
@@ -4,4 +4,5 @@ DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
 make_private_url() {
     local repodir=$1
     local reponame=$2
+    local url="ssh://${PRIVATE_HOST}:${PRIVATE_POST}/${repodir}/${reponame}.git"
 }
