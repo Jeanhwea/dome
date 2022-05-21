bc3775b0edb264284c74fe3bad31db85dc8611b8
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-22 02:59:13 +0800

    update

--- script/git-init-private-repository.sh
+++ script/git-init-private-repository.sh
@@ -1,8 +1,18 @@
 DOME_BASE=`cd $(dirname $(readlink -f $0)); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
+PRIVATE_USER=${PRIVATE_USER:="root"}
+PRIVATE_HOST=${PRIVATE_HOST:="localhost"}
+PRIVATE_PORT=${PRIVATE_PORT:="22"}
+
 make_private_url() {
     local repodir=$1
     local reponame=$2
-    local url="ssh://${PRIVATE_HOST}:${PRIVATE_POST}/${repodir}/${reponame}.git"
+    local url="ssh://${PRIVATE_HOST}:${PRIVATE_PORT}/${repodir}/${reponame}.git"
 }
+
+get_remote_repodir() {
+    echo ssh ${PRIVATE_USER}@${PRIVATE_HOST}:${PRIVATE_PORT} echo '$HOME'
+}
+
+get_remote_repodir
