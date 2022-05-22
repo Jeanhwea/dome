0352c82cfb245edda163a18ffcce57807b2f5f79
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-22 14:30:16 +0800

    timestamp: 2022-05-22 14:30:16

--- script/golang-format-source-here.sh
+++ script/golang-format-source-here.sh
@@ -6,7 +6,7 @@ dome_format_golans_source() {
     local files=$(find . -iname '*.go' -not -path "./*_gen/*")
     for file in ${files[@]}; do
         logi "format $file"
-        dome_exec "sed -i '/^import/,/^\s*)/ { /^\s*$/ d; }' $file"
+        dome_exec "sed -i '/^import (/,/^\s*)/ { /^\s*$/ d; }' $file"
         dome_exec "goimports -w $file"
     done
 }
