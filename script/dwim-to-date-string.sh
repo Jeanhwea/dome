1c752af4bac1bdbd3eb321abe7ab92ef77d9f452
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-22 23:05:39 +0800

    add: 添加未知时间格式字符串提示

--- script/dwim-to-date-string.sh
+++ script/dwim-to-date-string.sh
@@ -4,11 +4,14 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 dwim_print_date_str() {
     local str=${*/T/}
 
-    local pattern='^[0-9]+$'
-    if [[ $str =~ $pattern ]]; then
+    local pattern1='^[0-9]+$'
+    local pattern2='^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
+    if [[ $str =~ $pattern1 ]]; then
         ts_to_str $str
-    else
+    elif [[ $str =~ $pattern2 ]]; then
         str_to_ts $str
+    else
+        logw "Unknown DateTime String: $*"
     fi
 }
 
