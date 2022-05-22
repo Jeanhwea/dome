f12fd48a70878104b8dd784e7fa350ea052273c4
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-22 16:06:51 +0800

    update: 添加颜色判断

--- common/symbol-colors.sh
+++ common/symbol-colors.sh
@@ -1,17 +1,16 @@
 # Terminal Colors
-CLRRED="\033[31m"
-CLRGRN="\033[32m"
-CLRYLW="\033[33m"
-CLRBLU="\033[34m"
-CLRMGA="\033[35m"
-CLRRST="\033[0m"
+CLRRED=""
+CLRGRN=""
+CLRYLW=""
+CLRBLU=""
+CLRMGA=""
+CLRRST=""
 
-
-if [ "X$SHELL" = "X/bin/bash" ]; then
-    CLRRED=""
-    CLRGRN=""
-    CLRYLW=""
-    CLRBLU=""
-    CLRMGA=""
-    CLRRST=""
+if [ $DOME_COLOR = "yes" ]; then
+    CLRRED="\033[31m"
+    CLRGRN="\033[32m"
+    CLRYLW="\033[33m"
+    CLRBLU="\033[34m"
+    CLRMGA="\033[35m"
+    CLRRST="\033[0m"
 fi
