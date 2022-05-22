ecacccfb9663fd0ab54765f4d6123dd69dbcf10c
Jinghui Hu <hujinghui@buaa.edu.cn>
2022-05-22 22:46:17 +0800

    update: 添加时间处理函数

--- common/helper-time.sh
+++ common/helper-time.sh
@@ -1,6 +1,16 @@
 ts_to_str() {
     local ts=$*
-    date -d @$ts +"%Y-%m-%d %H:%M:%S"
+    local fmt="%Y-%m-%d %H:%M:%S"
+    date -d @$ts +"$fmt" 2>/dev/null || date -r $ts +"$fmt"
+}
+
+str_to_ts() {
+    local str=$*
+    local fmt="%Y-%m-%d %H:%M:%S"
+    date --date="$str" +"%s" 2>/dev/null || date -jf "$fmt" "$str" +"%s"
 }
 
 # ts_to_str 1650430795
+# str_to_ts 2022-04-20 12:59:55
+# str_to_ts 2022-06-20
+# ts_to_str 1655654400
