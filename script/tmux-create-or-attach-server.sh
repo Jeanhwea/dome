7d8357d469fa1ea69192b846963cb9c48baba726
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-11-03 19:17:58 +0800

    feat: create or attach tmux session

---
+++ script/tmux-create-or-attach-server.sh
@@ -0,0 +1,9 @@
+DOME_BASE=`cd $(dirname $0); cd ..; pwd`
+. $DOME_BASE/common/common.sh
+
+dome_tmux_create_or_attach() {
+    local session='Work'
+    tmux new-session -A -s $session
+}
+
+dome_tmux_create_or_attach $*
