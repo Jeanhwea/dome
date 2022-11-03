70a8dfa309e3da37642160bef682c6ee8ab76e15
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-11-03 19:19:16 +0800

    feat: 整理命令的别名

--- env/setup-alias.sh
+++ env/setup-alias.sh
@@ -1,11 +1,18 @@
 DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
-alias e='emacs -nw'
-alias em='emacsclient --no-wait'
+# Misc
 alias cbc='dome cl'
 alias dt='dm date'
 
+# Emacs
+alias e='emacs -nw'
+alias em='emacsclient --no-wait'
+
+# Makefile
 alias fmt='make format'
 alias gen='make generate'
 alias m='make'
+
+# Tmux
+alias ta='dm tmux-create-or-attach-server'
