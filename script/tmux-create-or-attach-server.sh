774119498a50def23c9908d35d326227c2cdc505
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-11-03 19:28:02 +0800

    feat: 修改 tmux 默认名称为主机名

--- script/tmux-create-or-attach-server.sh
+++ script/tmux-create-or-attach-server.sh
@@ -2,7 +2,8 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 dome_tmux_create_or_attach() {
-    local session="$HOST"
+    local session=$(hostname | cut -c -8)
+    local session=${session:-Work}
     tmux new-session -A -s $session
 }
 
