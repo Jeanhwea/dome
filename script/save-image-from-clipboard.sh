9d83c5eabbbe688b08edbea9f052e4d2a6dd07c5
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-11-05 22:19:44 +0800

    feat: 添加保存剪切板上的图片

---
+++ script/save-image-from-clipboard.sh
@@ -0,0 +1,20 @@
+DOME_BASE=`cd $(dirname $0); cd ..; pwd`
+. $DOME_BASE/common/common.sh
+
+save_image_from_clipboard() {
+    local proj=$(git rev-parse --show-toplevel)
+    local shim=$(date +"static/image/%Y/%m%d")
+    local name=$(date +"%H%M%S.png")
+
+
+    local dir="$proj/$shim"
+    [ -d $dir ] || mkdir -p $dir
+
+    local file="$dir/$name"
+
+    pngpaste $file
+
+    echo "[[file:$shim/$name]]" | pbcopy
+}
+
+save_image_from_clipboard $*
