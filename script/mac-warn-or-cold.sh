af2a05a48b83799bd64ecaaaebbcad6b4bf94692
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-12-13 01:13:31 +0800

    fix: 修改条件

--- script/mac-warn-or-cold.sh
+++ script/mac-warn-or-cold.sh
@@ -4,11 +4,11 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 mac_warn_or_cold() {
     local count=$(pgrep '^yes' | wc -l)
     if [ $count -gt 0 ]; then
+        pkill -9 '^yes'
+    else
         for i in $(seq `sysctl -n hw.ncpu`); do
             yes > /dev/null &
         done
-    else
-        pkill -9 '^yes'
     fi
 }
 
