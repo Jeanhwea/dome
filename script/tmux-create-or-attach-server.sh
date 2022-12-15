6104a5cab9608879d59336ae799d1a18a389fee0
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-12-15 16:29:31 +0800

    feat: tmux 添加 HOSTSHORT 环境变量

--- script/tmux-create-or-attach-server.sh
+++ script/tmux-create-or-attach-server.sh
@@ -2,8 +2,9 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 dome_tmux_create_or_attach() {
-    local session=$(hostname | cut -c -8)
-    local session=${session:-Work}
+    local short=$(hostname | cut -c -8)
+    local short=${short:-Work}
+    local session=${HOSTSHORT:=$short}
     tmux new-session -A -s $session
 }
 
