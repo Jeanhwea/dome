9000e35f63dc1002e9b3116b20f93e386af41533
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-02-01 11:42:51 +0800

    添加代码

--- script/mysql-trace-extract.sh
+++ script/mysql-trace-extract.sh
@@ -2,8 +2,16 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 dome_trace_extract() {
-    local conn_id=$1
-    dome_exec grep /tmp/mysqld.trace | grep "^T@${conn_id}"
+    local conn_id=0
+    if [ $# -gt 0 ]; then
+        local conn_id=$1
+    fi
+
+    local shim=$(date +"%Y%m%d-%H%M%S")
+    local prefix="/tmp/trace-${conn_id}-"
+    local out="${prefix}${shim}.txt"
+    echo $out
+    tail -f /tmp/mysqld.trace | grep "^T@${conn_id}" | tee $out
 }
 
-dome_trace_extract
+dome_trace_extract $*
