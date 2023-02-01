12e8a5e9a1bf0a6c9d23db3eed53887642868c0e
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-02-01 21:18:22 +0800

    add

--- script/mysql-trace-extract.sh
+++ script/mysql-trace-extract.sh
@@ -11,7 +11,8 @@ dome_trace_extract() {
     local prefix="/tmp/trace-${conn_id}-"
     local outfile="${prefix}${shim}.txt"
     echo $outfile
-    tail -f /tmp/mysqld.trace | grep "^T@${conn_id}" | sed 's/^T[^:]*: //' | tee $outfile
+    tail -f /tmp/mysqld.trace | grep "^T@${conn_id}" | sed 's/^T[^:]*: //' | sed 's/| /\t/g' | tee $outfile
+    # sed 's/^T[^:]*: /| /' | awk '{if (/^[| ]+(>|<)/) {gsub(/\| /,"*",$0); print} else {gsub(/\| /,"",$0); print}}' | sed 's/*</* </;s/*>/* >/'
 }
 
 dome_trace_extract $*
