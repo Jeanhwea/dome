16744342245c5ace49369caadc8a9da1962efddb
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-02-10 22:02:17 +0800

    修改 MySQL 的工具

--- script/mysql-trace-extract.sh
+++ script/mysql-trace-extract.sh
@@ -8,11 +8,11 @@ dome_trace_extract() {
     fi
 
     local shim=$(date +"%Y%m%d-%H%M%S")
-    local prefix="/tmp/trace-${conn_id}-"
-    local outfile="${prefix}${shim}.txt"
-    echo $outfile
-    tail -f /tmp/mysqld.trace | grep "^T@${conn_id}" | sed 's/^T[^:]*: //' | sed 's/| /\t/g' | tee $outfile
+    local outfile="$HOME/${shim}-mysql-trace-${conn_id}.txt"
+    # tail -f /tmp/mysqld.trace | grep "^T@${conn_id}" | sed 's/^T[^:]*: //' | sed 's/| /\t/g' | tee $outfile
     # sed 's/^T[^:]*: /| /' | awk '{if (/^[| ]+(>|<)/) {gsub(/\| /,"*",$0); print} else {gsub(/\| /,"",$0); print}}' | sed 's/*</* </;s/*>/* >/'
+    grep "^T@${conn_id}:" /tmp/mysqld.trace | sed 's/^T[^:]*: //;s/| /\t/g' > "$outfile"
+    logi "Save to $outfile"
 }
 
 dome_trace_extract $*
