999e572055bbedefe44e3521aa6fb2e7e32915f3
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-02-10 21:49:00 +0800

    ss

--- script/git-new-branch.sh
+++ script/git-new-branch.sh
@@ -1,7 +1,7 @@
 DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
-dome_new_branch_from_master() {
+dome_new_branch() {
     local name=$(date +'%Y%m%d')
     dome_exec git checkout -b $name
 }
