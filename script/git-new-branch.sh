333ae0cb4c773d05f07ad97f578c1e820bdffa83
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-02-10 22:03:41 +0800

    添加新建分支的逻辑

--- script/git-new-branch.sh
+++ script/git-new-branch.sh
@@ -2,8 +2,13 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 dome_new_branch() {
-    local name=$(date +'%Y%m%d')
-    dome_exec git checkout -b $name
+    local sub="A"
+    if [ $# -gt 0 ]; then
+        local sub=$1
+    fi
+
+    local name=$(date +'%m%d')
+    dome_exec git checkout -b $name$sub
 }
 
 dome_new_branch
