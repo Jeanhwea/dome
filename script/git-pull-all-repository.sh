95e1c8efb2613fce77b0bdeb7e53cc25feca1a4b
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2023-03-31 20:12:05 +0800

    feat: 添加 git 下载多个代码仓库

---
+++ script/git-pull-all-repository.sh
@@ -0,0 +1,12 @@
+DOME_BASE=`cd $(dirname $0); cd ..; pwd`
+. $DOME_BASE/common/common.sh
+
+dome_pull_all_repository() {
+    local base=${1:-$HOME/work}
+    for repo in `find $base -type d -iname '.git' -maxdepth 3`; do
+        local dir=${repo//.git/}
+        dome_exec git -C $dir pull
+    done
+}
+
+dome_pull_all_repository $*
