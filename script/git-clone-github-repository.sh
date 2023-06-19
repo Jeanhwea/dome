9a2b4550bb6889d695eb4b43a45792866e6e5247
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-06-19 10:02:04 +0800

    fix

--- script/git-clone-github-repository.sh
+++ script/git-clone-github-repository.sh
@@ -9,7 +9,7 @@ clone_my_github_repository() {
     fi
 
     local repo=$1
-    dome_exec dm git-clone-repository-to-local git@github.com:jeanhwea/${repo}.git
+    dome_exec dm git-clone-repository-to-local git@github.com:Jeanhwea/${repo}.git
 }
 
 clone_my_github_repository $*
