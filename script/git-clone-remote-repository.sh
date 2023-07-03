b1acf6f2e8f692c5767e0551a43f233983531eda
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-07-03 08:08:06 +0800

    完成添加 cl 命令，直接复制远端库

--- script/git-clone-remote-repository.sh
+++ script/git-clone-remote-repository.sh
@@ -3,24 +3,24 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 
 clone_my_remote_repository() {
     if [ $# -lt 2 ]; then
-        echo 'usage: clg <remote> <repo>'
+        echo 'usage: cl <remote> <repo>'
         exit 1
         return
     fi
 
     local remote=$1
     local repo=$2
+    # echo $remote $repo
 
-    if [ X"$remote" == "github" ]; then
+    if [ X"$remote" == X"github" ]; then
         dome_exec dm git-clone-repository-to-local git@github.com:Jeanhwea/${repo}.git
-    elif [ X"$remote" == "mtiisl" ]; then
+    elif [ X"$remote" == X"mtiisl" ]; then
         local url=ssh://git@mtiisl.cn:2222/hujinghui/${repo}.git
-        if [ X"$repo" == *'/'* ]; then
+        if [[ X"$repo" == *'/'* ]]; then
             url=ssh://git@mtiisl.cn:2222/${repo}.git
         fi
         dome_exec dm git-clone-repository-to-local $url
     fi
-
 }
 
 clone_my_remote_repository $*
