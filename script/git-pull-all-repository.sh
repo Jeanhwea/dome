16635310aaefe4a7547710065130f125d095cc50
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-21 11:10:01 +0800

    update: 修改 code 路径

--- script/git-pull-all-repository.sh
+++ script/git-pull-all-repository.sh
@@ -2,7 +2,7 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 dome_pull_all_repositories() {
-    local base=${1:-$HOME/work}
+    local base=${1:-$DOME_CODE_DIR}
     for repo in `find $base -type d -iname '.git' -maxdepth 3`; do
         # local dir=`dirname $repo`
         local dir=${repo///.git/}
