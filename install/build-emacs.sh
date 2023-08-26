2fbab5e99f0a72485b85f227b06bed4cb16ef428
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-08-26 21:52:09 +0800

    add build emacs

--- install/build-emacs.sh
+++ install/build-emacs.sh
@@ -19,13 +19,14 @@ dome_build_emacs_package() {
     cd ${BUILDDIR}/emacs-${VERSION}
 
     echo "building..."
-    ./configure
-    make -j$(nproc)
+    ./autogen.sh    | tee stage01_autogen.log
+    ./configure     | tee stage02_configure.log
+    make -j$(nproc) | tee stage03_make.log
 }
 
 dome_install_emacs() {
     cd ${BUILDDIR}/emacs-${VERSION}
-    sudo make install
+    sudo make install | tee stage04_install.log
 }
 
 
