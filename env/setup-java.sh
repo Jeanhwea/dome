f60af195c7297502b8563472a86a6cb597a9df1d
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-03-29 16:26:25 +0800

    update

--- env/setup-java.sh
+++ env/setup-java.sh
@@ -1,4 +1,5 @@
 export JAVA_HOME=${JAVA_HOME:-/usr/local/java/jdk}
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar${CLASSPATH:+:${CLASSPATH}}
 export MAVEN_HOME=${MAVEN_HOME:/usr/local/java/maven}
+
+# export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar${CLASSPATH:+:${CLASSPATH}}
 export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
