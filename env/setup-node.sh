f7e5dfdf28eae36a45c31ad81ae4540c896b7e18
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-03-29 16:19:06 +0800

    update

--- env/setup-node.sh
+++ env/setup-node.sh
@@ -1,3 +1,3 @@
 # npm config set prefix '~/.local/node_modules'
-export NODE_MODULES_PATH=$HOME/.local/node_modules
+export NODE_MODULES_PATH=${NODE_MODULES_PATH:-$HOME/.local/node_modules}
 export PATH=$NODE_MODULES_PATH/bin:$PATH
