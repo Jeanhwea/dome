f686d9f1259d45f228f0c247aea3302a22824714
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-23 21:50:26 +0800

    add nextjs scripts

--- script/app-new-nextjs.sh
+++ script/app-new-nextjs.sh
@@ -1,15 +1,15 @@
 DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
-dome_new_app_reactapp() {
+dome_new_app_nextjs() {
     local app="app01"
     if [ $# -gt 0 ]; then
         local app=$1
     fi
 
-    dome_exec npx create-react-app "$app"
+    dome_exec npx create-next-app@latest "$app"
 
     # dome_exec "cd $app && git init && git add . && git commit -m init"
 }
 
-dome_new_app_reactapp $*
+dome_new_app_nextjs $*
