6bfa7b54b2ab8c04fef94dfce049dbeba14ef6bc
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-24 22:17:16 +0800

    update

--- env/setup-database.sh
+++ env/setup-database.sh
@@ -2,8 +2,8 @@ DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
 # mysql
-export PATH="/opt/mysql/bin:$PATH"
 export MYSQL_HOME="/opt/mysql"
+export PATH="$MYSQL_HOME/bin:$PATH"
 
 # postgres
 export PGDATA="/opt/pgsql/data"
