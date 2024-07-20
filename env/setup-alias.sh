6a341141394f33354950ead346621e3a2c9e1a48
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-07-20 15:27:31 +0800

    update: 调整 cargo run 的级别

--- env/setup-alias.sh
+++ env/setup-alias.sh
@@ -50,7 +50,8 @@ alias logf='make logf'
 # alias for cargo
 # alias cr='RUST_LOG=info RUST_BACKTRACE=1 cargo run --quiet'
 # alias ct='RUST_LOG=info RUST_BACKTRACE=1 cargo test --quiet -- --show-output'
-alias cr='RUST_LOG=debug RUST_BACKTRACE=1 cargo run'
+# alias cr='RUST_LOG=debug RUST_BACKTRACE=1 cargo run'
+alias cr='RUST_LOG=info RUST_BACKTRACE=1 cargo run'
 alias ct='RUST_LOG=info RUST_BACKTRACE=1 cargo test -- --show-output'
 alias cb='cargo build'
 
