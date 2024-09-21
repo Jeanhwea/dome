620c2fd86e9900d4b5db6855d6e3b555d676ec02
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-09-21 12:56:46 +0000

    add: 添加安装 Java LSP 的脚本

--- install/ins-java-lsp.sh
+++ install/ins-java-lsp.sh
@@ -2,4 +2,23 @@
 DOME_BASE=`cd $(dirname $0); cd ..; pwd`
 . $DOME_BASE/common/common.sh
 
-wget -c https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.39.0/jdt-language-server-1.39.0-202408291433.tar.gz
+DOWNLINK=https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.39.0/jdt-language-server-1.39.0-202408291433.tar.gz
+TARBALL=jdt-language-server-1.39.0-202408291433.tar.gz
+JDTDIR=~/bin/jdt
+JDTBIN=~/bin/jdtls
+
+wget -c $DOWNLINK -o $TARBALL
+mv $TARBALL /tmp
+
+mkdir -p $JDTDIR
+tar xzvf -C $JDTDIR /tmp/$TARBALL
+
+cat >> $JDTBIN <<\EOF
+#!/usr/bin/env sh
+export JAVA_HOME=$JAVA_HOME
+export PATH=$JAVA_HOME/bin:$PATH
+
+exec $HOME/bin/jdt/bin/jdtls $*
+EOF
+
+chmod +x $JDTBIN
